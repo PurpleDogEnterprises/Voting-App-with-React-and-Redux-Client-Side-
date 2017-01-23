b9d22650d670313de2a908c859be98bfe79b025e
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-01-23 09:23:15 -0500

    Update Voting_spec.jsx

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -27,9 +27,9 @@ describe('Voting', () => {
   	);
 	const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
-	  expect(buttons.length).to.equal(2);
-	  expect(buttons[0].hasAttribute('disabled')).to.equal(true);
-	  expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+	expect(buttons.length).to.equal(2);
+	expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+	expect(buttons[1].hasAttribute('disabled')).to.equal(true);
   });
 
   it('adds label to the voted entry', () => {
@@ -40,6 +40,18 @@ describe('Voting', () => {
   	const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
   	expect(buttons[0].textContent).to.contain('Voted');
+  });
+
+  it('renders just the winner when there is one', () => {
+  const component = renderIntoDocument(
+    <Voting winner="Trainspotting" />
+  	);
+  	const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+  	expect(buttons.length).to.equal(0);
+
+  	const winner = ReactDOM.findDOMNode(component.refs.winner);
+  	expect(winner).to.be.ok;
+  	expect(winner.textContent).to.contain('Trainspotting');
   });
 
 });
