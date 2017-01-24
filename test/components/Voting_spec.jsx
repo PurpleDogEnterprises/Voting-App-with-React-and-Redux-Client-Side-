14d62d6c88f29e92489ab67cce860a66aebb24f8
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-01-24 08:57:09 -0500

    Add import list

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -4,6 +4,7 @@ import {
   renderIntoDocument,
   scryRenderedDOMComponentsWithTag
 } from 'react-addons-test-utils';
+import {List} from 'immutable';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
