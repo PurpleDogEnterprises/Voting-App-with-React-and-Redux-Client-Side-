1b15a2adfba1cf70727a1149f47ab7b7ef3644b8
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-01-26 11:33:07 -0500

    Update Results_spec.jsx

--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithClass
+  scryRenderedDOMComponentsWithClass,
+  Simulate
 } from 'react-addons-test-utils';
 import {List, Map} from 'immutable';
 import Results from '../../src/components/Results';
