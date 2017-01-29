f93d6ff370018b2887568a8f19985747b07ee601
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-01-29 11:10:11 -0500

    Now exports two components

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
 
@@ -13,3 +14,12 @@ export const Voting = React.createClass({
     </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+export const VotingContainer = connect(mapStateToProps)(Voting);
